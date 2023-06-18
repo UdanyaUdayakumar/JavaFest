1bee9fabb497d09051aab01954256609020bd0b0
Udayakumar KV <76675509+UdanyaUdayakumar@users.noreply.github.com>
2023-06-18 17:52:34 +0530

    Day_03 Assignments

---
+++ day_03/ASCIICodeAlphabets.java
@@ -0,0 +1,177 @@
+package day_03;
+import java.util.*;
+public class ASCIICodeAlphabets {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scanner = new Scanner(System.in);
+		char alphabet;
+		System.out.println("Enter the character From A to Z or a to z.");
+		alphabet = scanner.next().charAt(0);
+		//char alphabet = 'h';
+		
+		switch(alphabet)
+		{
+		case 'A': 
+			System.out.println("ASCII value is 65");
+			break;
+		case 'B': 
+			System.out.println("ASCII value is 66");
+			break;
+		case 'C': 
+			System.out.println("ASCII value is 67");
+			break;
+		case 'D': 
+			
+			System.out.println("ASCII value is 68");
+			break;
+		case 'E': 
+			System.out.println("ASCII value is 69");
+			break;
+		case 'F': 
+			System.out.println("ASCII value is 70");
+			break;
+		case 'G': 
+			System.out.println("ASCII value is 71");
+			break;
+		case 'H': 
+			System.out.println("ASCII value is 72");
+			break;
+		case 'I': 
+			System.out.println("ASCII value is 73");
+			break;
+		case 'J': 
+			System.out.println("ASCII value is 74");
+			break;
+		case 'K': 
+			System.out.println("ASCII value is 75");
+			break;
+		case 'L': 
+			System.out.println("ASCII value is 76");
+			break;
+		case 'M': 
+			System.out.println("ASCII value is 77");
+			break;
+		case 'N': 
+			System.out.println("ASCII value is 78");
+			break;
+		case 'O': 
+			System.out.println("ASCII value is 79");
+			break;
+		case 'P': 
+			System.out.println("ASCII value is 80");
+			break;
+		case 'Q': 
+			System.out.println("ASCII value is 81");
+			break;
+		case 'R': 
+			System.out.println("ASCII value is 82");
+			break;
+		case 'S': 
+			System.out.println("ASCII value is 83");
+			break;
+		case 'T': 
+			System.out.println("ASCII value is 84");
+			break;
+		case 'U': 
+			System.out.println("ASCII value is 85");
+			break;
+		case 'V': 
+			System.out.println("ASCII value is 86");
+			break;
+		case 'W': 
+			System.out.println("ASCII value is 87");
+			break;
+		case 'X': 
+			System.out.println("ASCII value is 88");
+			break;
+		case 'Y': 
+			System.out.println("ASCII value is 89");
+			break;
+		case 'Z': 
+			System.out.println("ASCII value is 90");
+			break;
+		case 'a': 
+			System.out.println("ASCII value is 97");
+			break;
+		case 'b': 
+			System.out.println("ASCII value is 98");
+			break;
+		case 'c': 
+			System.out.println("ASCII value is 99");
+			break;
+		case 'd': 
+			System.out.println("ASCII value is 100");
+			break;
+		case 'e': 
+			System.out.println("ASCII value is 101");
+			break;
+		case 'f': 
+			System.out.println("ASCII value is 102");
+			break;
+		case 'g': 
+			System.out.println("7ASCII value is 103");
+			break;
+		case 'h': 
+			System.out.println("ASCII value is 104");
+			break;
+		case 'i': 
+			System.out.println("ASCII value is 105");
+			break;
+		case 'j': 
+			System.out.println("ASCII value is 106");
+			break;
+		case 'k': 
+			System.out.println("ASCII value is 107");
+			break;
+		case 'l': 
+			System.out.println("ASCII value is 108");
+			break;
+		case 'm': 
+			System.out.println("ASCII value is 109");
+			break;
+		case 'n': 
+			System.out.println("ASCII value is 110");
+			break;
+		case 'o': 
+			System.out.println("ASCII value is 111");
+			break;
+		case 'p': 
+			System.out.println("ASCII value is 112");
+			break;
+		case 'q': 
+			System.out.println("ASCII value is 113");
+			break;
+		case 'r': 
+			System.out.println("ASCII value is 114");
+			break;
+		case 's': 
+			System.out.println("ASCII value is 115");
+			break;
+		case 't': 
+			System.out.println("ASCII value is 116");
+			break;
+		case 'u': 
+			System.out.println("ASCII value is 117");
+			break;
+		case 'v': 
+			System.out.println("ASCII value is 118");
+			break;
+		case 'w': 
+			System.out.println("ASCII value is 119");
+			break;
+		case 'x': 
+			System.out.println("ASCII value is 120");
+			break;
+		case 'y': 
+			System.out.println("ASCII value is 121");
+			break;
+		case 'z': 
+			System.out.println("ASCII value is 122");
+			break;
+		default :
+			System.out.println("Enter valid character From A to Z or a to z.");
+		}
+	}
+
+}
